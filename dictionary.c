b15fd59be22d09923301c920fd52365d68e8b7f1
Kevin-GR <kegure112@gmail.com>
2024-09-20 10:19:06 -0600

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -1,5 +1,11 @@
 #include "dictionary.h"
 
-void initializeDataDictionary() {
+void initializeDataDictionary(const char *dictionaryName) {
+    long mainHeader = EMPTY_POINTER;
     printf("Initializing Data Dictionary...\n");
+
+    FILE *dictionary = fopen (dictionaryName, "W+");
+
+    fwrite (&mainHeader, sizeof(mainHeader))
+    
 }
