12584430b7d0c63f6636cee9b80851aa141d5ddb
Kevin-GR <kegure112@gmail.com>
2024-09-20 10:36:19 -0600

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -2,10 +2,20 @@
 
 void initializeDataDictionary(const char *dictionaryName) {
     long mainHeader = EMPTY_POINTER;
+    int stringsToSave =0;
     printf("Initializing Data Dictionary...\n");
 
     FILE *dictionary = fopen (dictionaryName, "W+");
+    fwrite (&mainHeader, sizeof(mainHeader)1, dictionary);
+    printf("cuantas cadenas quieres guardar");
+    scanf("%d", &stringsToSave);
 
-    fwrite (&mainHeader, sizeof(mainHeader))
-    
+    for(int counter=0; counter< stringsToSave; counter++)
+    {
+        NODE currentNode;
+        currentNode.next =EMPTY_POINTER;
+        printf("enter a value for node #%d", counter);
+        fgets(&(currentNode.value),sizeof(currentNode.value), dictionary);    
+    }
+    return EXIT_SUCCESS;
 }
