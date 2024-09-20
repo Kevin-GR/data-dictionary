1a8b5c33a5dc952b65d8d3f94aeded970a7852e0
Kevin-GR <kegure112@gmail.com>
2024-09-20 10:23:31 -0600

    Update dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -1,7 +1,17 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
-
+ #define   EMPTY_POINTER -1
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+  char value [50];
+  struct Node *next;
+  // in memory pointer
+  // file pointer
+  long next; 
+}NODE;
 
 void initializeDataDictionary();
 
